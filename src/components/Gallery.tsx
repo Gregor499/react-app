b8d5496863b5424b2397d1c9df2c6c972f3374c8
Gregor499 <postfach@gregor-dirks.de>
2022-06-09 14:18:22 +0200

    Namenssuche hinzugefügt

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,13 +1,25 @@
 import characters from '../characters.json';
 import GalleryItem from './GalleryItem';
 import './Gallery.css';
+import {useState} from "react";
 
 export default function Gallery() {
-    const components = characters.map(c => <GalleryItem character={{name: c.name, image: c.image, status: c.status, species: c.species}} />)
+    //const components = characters.map(c => <GalleryItem character={{name: c.name, image: c.image, status: c.status, species: c.species}} />)
+
+    const [name, setName] = useState("");
+
+    const components = characters
+        .filter(c => c.name.toLowerCase().includes(name.toLowerCase()))
+        .map(c => <GalleryItem character={{name: c.name, image: c.image, status: c.status, species: c.species}} />)
 
     return (
         <div className="gallery">
-            {components}
+            <div className="search">
+                Search for Name: <input type="text" value={name} onChange={ev => setName(ev.target.value)}/>
+            </div>
+            <div className="gallery">
+                {components}
+            </div>
         </div>
     )
 }
